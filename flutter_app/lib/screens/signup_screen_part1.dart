1d0a770fc42d11f2cd581acee076703579c1f06a
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 03:06:00 +0200

    Edited sinup screen by adding logo and white rounded box

--- flutter_app/lib/screens/signup_screen_part1.dart
+++ flutter_app/lib/screens/signup_screen_part1.dart
@@ -18,56 +18,80 @@ class SignUpScreenPart1State extends State<SignUpScreenPart1> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFC7D3E1),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'First Name'),
-                validator: (value) =>
-                    value!.isEmpty ? 'Enter your first name' : null,
-                onSaved: (value) => widget.formData.firstName = value!,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 25),
+            Image.asset('assets/logo.png', height: 200),
+            const SizedBox(height: 25),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
               ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Last Name'),
-                validator: (value) =>
-                    value!.isEmpty ? 'Enter your last name' : null,
-                onSaved: (value) => widget.formData.lastName = value!,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Username'),
-                validator: (value) =>
-                    value!.isEmpty ? 'Enter a username' : null,
-                onSaved: (value) => widget.formData.username = value!,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) => value!.isEmpty || !value.contains('@')
-                    ? 'Enter a valid email'
-                    : null,
-                onSaved: (value) => widget.formData.email = value!,
-              ),
-              ElevatedButton(
-                child: const Text('Next'),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Navigate to part 2 of the sign-up screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            SignUpScreenPart2(formData: widget.formData),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'First Name'),
+                        validator: (value) =>
+                            value!.isEmpty ? 'Enter your first name' : null,
+                        onSaved: (value) => widget.formData.firstName = value!,
                       ),
-                    );
-                  }
-                },
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Last Name'),
+                        validator: (value) =>
+                            value!.isEmpty ? 'Enter your last name' : null,
+                        onSaved: (value) => widget.formData.lastName = value!,
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Username'),
+                        validator: (value) =>
+                            value!.isEmpty ? 'Enter a username' : null,
+                        onSaved: (value) => widget.formData.username = value!,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) =>
+                            value!.isEmpty || !value.contains('@')
+                                ? 'Enter a valid email'
+                                : null,
+                        onSaved: (value) => widget.formData.email = value!,
+                      ),
+                      ElevatedButton(
+                        child: const Text('Next'),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            // Navigate to part 2 of the sign-up screen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignUpScreenPart2(
+                                    formData: widget.formData),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
