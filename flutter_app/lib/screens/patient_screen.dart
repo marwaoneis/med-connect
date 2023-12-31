069085365d1958f1a8020b3d98b80da403c125d5
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 18:44:19 +0200

    Created the appointment card widget

--- flutter_app/lib/screens/patient_screen.dart
+++ flutter_app/lib/screens/patient_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../widgets/top_bar.dart';
 import '../widgets/search_bar.dart';
 import '../widgets/menu_grid.dart';
+import '../widgets/appointment_card.dart';
+import '../widgets/pharmacy_list.dart';
 
 class PatientScreen extends StatelessWidget {
   const PatientScreen({super.key});
@@ -14,6 +16,10 @@ class PatientScreen extends StatelessWidget {
         children: const [
           CustomSearchBar(),
           MenuGrid(),
+          SizedBox(height: 10),
+          AppointmentCard(),
+          SizedBox(height: 10),
+          PharmacyList(),
         ],
       ),
     );
---
+++ flutter_app/lib/widgets/pharmacy_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PharmacyList extends StatelessWidget {
+  const PharmacyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with a ListView.builder for actual pharmacy list
+    return Container(height: 100, color: Colors.yellow);
+  }
+}
