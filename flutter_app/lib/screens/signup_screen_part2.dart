d08129103ad29bc2f4a1c6588c95422ea8e176a6
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 05:45:40 +0200

    Added back button functionality on signup scree

--- flutter_app/lib/screens/signup_screen_part2.dart
+++ flutter_app/lib/screens/signup_screen_part2.dart
@@ -107,147 +107,177 @@ class SignUpScreenPart2State extends State<SignUpScreenPart2> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFC7D3E1),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 25),
-            Image.asset('assets/logo.png', height: 200),
-            const SizedBox(height: 25),
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25),
-                  topRight: Radius.circular(25),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const SizedBox(height: 15),
-                      const Text(
-                        'Signup',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Divider(
-                        color: const Color(0xFF71717A),
-                        thickness: 1,
-                        indent: 7,
-                        height: 6,
-                        endIndent: MediaQuery.of(context).size.width - 95,
-                      ),
-                      const SizedBox(height: 15),
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'Address'),
-                        onSaved: (value) => widget.formData.address = value!,
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: 'Phone Number'),
-                        keyboardType: TextInputType.phone,
-                        onSaved: (value) => widget.formData.phone = value!,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Date of Birth (yyyy-MM-dd)',
-                          errorText: _dateOfBirthError,
-                        ),
-                        keyboardType: TextInputType.datetime,
-                        onSaved: (value) {
-                          setState(() => _dateOfBirthError = null);
-                          if (value != null && value.isNotEmpty) {
-                            try {
-                              widget.formData.dateOfBirth =
-                                  DateTime.parse(value);
-                            } catch (e) {
-                              // Set the state to display an error message
-                              setState(() =>
-                                  _dateOfBirthError = 'Invalid date format');
-                              widget.formData.dateOfBirth = null;
-                            }
-                          } else {
-                            widget.formData.dateOfBirth = null;
-                          }
-                        },
-                        validator: (value) {
-                          if (value != null && value.isNotEmpty) {
-                            try {
-                              DateTime.parse(value);
-                              return null;
-                            } catch (e) {
-                              return 'Enter date in yyyy-MM-dd format';
-                            }
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'Gender'),
-                        onSaved: (value) => widget.formData.gender = value!,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Additional Info',
-                          hintText: 'Medical Info and Blood Group',
-                        ),
-                        onSaved: (value) {
-                          if (value != null) {
-                            widget.formData.additionalInfo =
-                                _parseAdditionalInfo(value);
-                          }
-                        },
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: 'Password'),
-                        obscureText: true,
-                        validator: (value) => value!.isEmpty || value.length < 6
-                            ? 'Enter a longer password'
-                            : null,
-                        onSaved: (value) => widget.formData.password = value!,
-                      ),
-                      const SizedBox(height: 35),
-                      _isLoading
-                          ? const CircularProgressIndicator()
-                          : ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(
-                                    0xFF0D4C92), // Button background color
-                                foregroundColor:
-                                    Colors.white, // Button text color
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(35),
-                                ),
-                                minimumSize: const Size(double.infinity,
-                                    50), // Setting the height to 40
-                              ),
-                              onPressed: _register,
-                              child: const Text(
-                                'Sign Up',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.normal),
-                              ),
+      // appBar: PreferredSize(
+      //   preferredSize: const Size.fromHeight(0),
+      //   child: AppBar(
+      //     backgroundColor:
+      //         Colors.transparent, // Set the AppBar background to transparent
+      //     elevation: 0,
+      //     leading: IconButton(
+      //       icon: const Icon(Icons.arrow_back, color: Colors.black),
+      //       onPressed: () => Navigator.of(context).pop(),
+      //     ),
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 25),
+                Image.asset('assets/logo.png', height: 200),
+                const SizedBox(height: 25),
+                Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const SizedBox(height: 15),
+                          const Text(
+                            'Signup',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
                             ),
-                      const SizedBox(height: 12),
-                    ],
+                          ),
+                          Divider(
+                            color: const Color(0xFF71717A),
+                            thickness: 1,
+                            indent: 7,
+                            height: 6,
+                            endIndent: MediaQuery.of(context).size.width - 95,
+                          ),
+                          const SizedBox(height: 15),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Address'),
+                            onSaved: (value) =>
+                                widget.formData.address = value!,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Phone Number'),
+                            keyboardType: TextInputType.phone,
+                            onSaved: (value) => widget.formData.phone = value!,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Date of Birth (yyyy-MM-dd)',
+                              errorText: _dateOfBirthError,
+                            ),
+                            keyboardType: TextInputType.datetime,
+                            onSaved: (value) {
+                              setState(() => _dateOfBirthError = null);
+                              if (value != null && value.isNotEmpty) {
+                                try {
+                                  widget.formData.dateOfBirth =
+                                      DateTime.parse(value);
+                                } catch (e) {
+                                  // Set the state to display an error message
+                                  setState(() => _dateOfBirthError =
+                                      'Invalid date format');
+                                  widget.formData.dateOfBirth = null;
+                                }
+                              } else {
+                                widget.formData.dateOfBirth = null;
+                              }
+                            },
+                            validator: (value) {
+                              if (value != null && value.isNotEmpty) {
+                                try {
+                                  DateTime.parse(value);
+                                  return null;
+                                } catch (e) {
+                                  return 'Enter date in yyyy-MM-dd format';
+                                }
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Gender'),
+                            onSaved: (value) => widget.formData.gender = value!,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Additional Info',
+                              hintText: 'Medical Info and Blood Group',
+                            ),
+                            onSaved: (value) {
+                              if (value != null) {
+                                widget.formData.additionalInfo =
+                                    _parseAdditionalInfo(value);
+                              }
+                            },
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) =>
+                                value!.isEmpty || value.length < 6
+                                    ? 'Enter a longer password'
+                                    : null,
+                            onSaved: (value) =>
+                                widget.formData.password = value!,
+                          ),
+                          const SizedBox(height: 35),
+                          _isLoading
+                              ? const CircularProgressIndicator()
+                              : ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(
+                                        0xFF0D4C92), // Button background color
+                                    foregroundColor:
+                                        Colors.white, // Button text color
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(35),
+                                    ),
+                                    minimumSize: const Size(double.infinity,
+                                        50), // Setting the height to 40
+                                  ),
+                                  onPressed: _register,
+                                  child: const Text(
+                                    'Sign Up',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.normal),
+                                  ),
+                                ),
+                          const SizedBox(height: 12),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          SafeArea(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.black),
+                onPressed: () => Navigator.of(context).pop(),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
