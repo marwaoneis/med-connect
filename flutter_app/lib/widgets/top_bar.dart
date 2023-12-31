c7ce884ec024cb1cc0ef954ee92b0f683abaf645
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 19:19:46 +0200

    Edited the navigation in top bar widget

--- flutter_app/lib/widgets/top_bar.dart
+++ flutter_app/lib/widgets/top_bar.dart
@@ -5,12 +5,17 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return Scaffold(
+        appBar: AppBar(
       title: const Text('Welcome Bernard'),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: () => Navigator.pushReplacementNamed(context, '/'),
+      ),
       actions: [
         IconButton(icon: const Icon(Icons.notifications), onPressed: () {})
       ],
-    );
+    ));
   }
 
   @override
