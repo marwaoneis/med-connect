0bc3e9b1ef617bef22346db7f30dab6f1531fd56
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 18:20:31 +0200

    Created the top bar widget

--- flutter_app/lib/screens/patient_screen.dart
+++ flutter_app/lib/screens/patient_screen.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
+import '../widgets/top_bar.dart';
 
 class PatientScreen extends StatelessWidget {
   const PatientScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Patient Dashboard'),
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.pushReplacementNamed(context, '/'),
-        ),
-      ),
+    return const Scaffold(
+      appBar: TopBar(),
     );
   }
 }
---
+++ flutter_app/lib/widgets/top_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget implements PreferredSizeWidget {
+  const TopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Welcome Bernard'),
+      actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
