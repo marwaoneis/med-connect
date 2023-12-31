072b648fcfab3750ca33fad15e5d8f2886ec35af
marwaoneis <marwa.oneis22@gmail.com>
2023-12-31 04:15:42 +0200

    Added the neccessary imports in login and signup screens

--- flutter_app/lib/screens/signup_screen_part1.dart
+++ flutter_app/lib/screens/signup_screen_part1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/login_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'signup_screen_part2.dart';
 import '../data/sign_up_form_data.dart';
 
@@ -90,6 +91,24 @@ class SignUpScreenPart1State extends State<SignUpScreenPart1> {
                                 : null,
                         onSaved: (value) => widget.formData.email = value!,
                       ),
+                      const SizedBox(height: 16.0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Already have an account? Login',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey, // Set the text color to grey
+                          ),
+                        ),
+                      ),
                       const SizedBox(height: 24.0),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
@@ -120,21 +139,88 @@ class SignUpScreenPart1State extends State<SignUpScreenPart1> {
                           }
                         },
                       ),
-                      const SizedBox(height: 16.0),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const LoginScreen(),
-                            ),
-                          );
-                        },
-                        child: Text('Already have an account? Login'),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Divider(color: Color(0xFF71717A)),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'OR',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Color(0xFF71717A),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(width: 5),
+                      Expanded(
+                        child: Divider(color: Color(0xFF71717A)),
                       ),
                     ],
                   ),
                 ),
+                SizedBox(height: 35),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement Facebook sign-in functionality
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF0D4C92),
+                        foregroundColor: const Color(0xFF4676ED),
+                        minimumSize: const Size(100, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Image.asset(
+                        'assets/facebook.png',
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF0D4C92),
+                        foregroundColor: Colors.grey,
+                        minimumSize: const Size(100, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(10), // Rounded corners
+                        ),
+                        side: const BorderSide(color: Colors.grey),
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/chrome.svg',
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement Apple sign-in functionality
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color(0xFF0D4C92), // Apple button color
+                        foregroundColor: Colors.black, // Icon color
+                        minimumSize: const Size(100, 50), // Square size
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(10), // Rounded corners
+                        ),
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/apple.svg',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
               ),
             ),
           ],
