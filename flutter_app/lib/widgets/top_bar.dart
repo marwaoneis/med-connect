4876c8876fc71e129b9c65c5e87d4d3caa036df6
marwaoneis <marwa.oneis22@gmail.com>
2024-01-01 17:49:10 +0200

    Edited the top bar

--- flutter_app/lib/widgets/top_bar.dart
+++ flutter_app/lib/widgets/top_bar.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
 
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
-  const TopBar({super.key});
+  final String firstName;
+  final String address;
+
+  const TopBar({required this.firstName, required this.address, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: const Text('Welcome Bernard'),
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.black),
-        onPressed: () => Navigator.pushReplacementNamed(context, '/'),
+    return AppBar(
+      backgroundColor:
+          const Color(0xFF0D4C92), // Set the background color as specified
+      shape: const ContinuousRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+      title: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome $firstName',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              address,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[300],
+              ),
+            ),
+          ],
+        ),
       ),
       actions: [
-        IconButton(icon: const Icon(Icons.notifications), onPressed: () {})
+        IconButton(
+          icon: const Icon(Icons.search, color: Colors.white),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: const Icon(Icons.notifications_none, color: Colors.white),
+          onPressed: () {},
+        ),
+        const SizedBox(width: 16), // For some padding at the end
       ],
-    ));
+      elevation: 0, // Removes shadow from the AppBar
+    );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize =>
+      const Size.fromHeight(140); // Set the AppBar height as specified
 }
