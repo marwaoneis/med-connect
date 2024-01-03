c02ef10e9ee45ce08ebb143c57c59f8e3182af95
marwaoneis <marwa.oneis22@gmail.com>
2024-01-03 21:20:07 +0200

    Created message list widget

--- flutter_app/lib/widgets/message_list.dart
+++ flutter_app/lib/widgets/message_list.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class MessageList extends StatelessWidget {
+  const MessageList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20, // Replace with your actual data length
+      itemBuilder: (context, index) {
+        return const ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/profile_placeholder.svg'),
+          ),
+          title: Text('Dr Name'),
+          subtitle: Text('Message Content'),
+          trailing: Text('Date/time'),
+        );
+      },
+    );
+  }
+}
