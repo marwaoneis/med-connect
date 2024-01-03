d628b571daa83ada3794833691288cdcd947028b
marwaoneis <marwa.oneis22@gmail.com>
2024-01-03 16:51:09 +0200

    Created config folder for requests

---
+++ flutter_app/lib/config/request_config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_app/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
+
+class RequestConfig {
+  static const url = 'http://10.0.2.2:3001';
+  static const Map<String, String> headers = {
+    'Content-Type': 'application/json'
+  };
+
+  Map<String, String> getHeaders(context) {
+    final token = Provider.of<Auth>(context, listen: false).getToken;
+    if (token != null) {
+      return {...headers, 'Authorization': 'Bearer $token'};
+    } else {
+      return headers;
+    }
+  }
+}
