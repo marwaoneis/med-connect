f5b1789ca7711583e67fb6963dfe7c72cf4cd5ec
marwaoneis <marwa.oneis22@gmail.com>
2024-01-04 03:25:47 +0200

    Created the request file in tools folder

---
+++ flutter_app/lib/tools/request.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../config/request_config.dart';
+
+//Sending http request
+Future sendRequest({
+  required String route,
+  method = "GET",
+  load,
+  context,
+}) async {
+  final url = Uri.http(RequestConfig.url, route);
+  final Map<String, String> headers = RequestConfig().getHeaders(context);
+
+  if (method == "GET") {
+    final response = await http.get(
+      url,
+      headers: headers,
+    );
+    final data = json.decode(response.body);
+    return data;
+  }
+
+  if (method == "POST") {
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: json.encode(load),
+    );
+    final data = json.decode(response.body);
+    return data;
+  }
+
+  if (method == "PUT") {
+    final response = await http.put(
+      url,
+      headers: headers,
+      body: json.encode(load),
+    );
+    final data = json.decode(response.body);
+    return data;
+  }
+
+  if (method == "DELETE") {
+    final response = await http.delete(
+      url,
+      headers: headers,
+      body: json.encode(load),
+    );
+    final data = json.decode(response.body);
+    return data;
+  }
+
+  return Future(() {
+    return "Not a valid method";
+  });
+}
