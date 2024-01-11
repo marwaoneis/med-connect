89013f9496938c9dd9d04c42cce5031d76998a98
marwaoneis <marwa.oneis22@gmail.com>
2024-01-12 00:33:50 +0200

    Backend <> Edited auth middleware

--- nodejs_server/middleware/auth.middleware.js
+++ nodejs_server/middleware/auth.middleware.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-  // Get the token from the request header
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
