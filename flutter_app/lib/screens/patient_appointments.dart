f7cfbbb055a8f8ebb062032ea3d8a84d412330b0
marwaoneis <marwa.oneis22@gmail.com>
2024-01-12 21:57:55 +0200

    Frontend <> Edited book appointment screen

--- flutter_app/lib/screens/patient_appointments.dart
+++ flutter_app/lib/screens/patient_appointments.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import '../widgets/doctor_card.dart';
+import '../widgets/top_bar_with_background.dart';
 
-class BookAppointmentPage extends StatelessWidget {
-  const BookAppointmentPage({super.key});
+class BookAppointmentScreen extends StatelessWidget {
+  const BookAppointmentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Patient Appointments'),
-      ),
-      body: const Center(
-        child: Text(
-          'Your Appointments',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: <Widget>[
+          TopBarWithBackground(
+            leadingContent: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            titleContent: const Text(
+              'Nearby Doctors',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            trailingContent: IconButton(
+              icon: const Icon(Icons.filter_list, color: Colors.white),
+              onPressed: () {
+                // TODO: Sort action
+              },
+            ),
           ),
-        ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return DoctorCard();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
