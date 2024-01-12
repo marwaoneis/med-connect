dfaf899b931ccce4cea927ee1f2e2c739a5dc7c9
marwaoneis <marwa.oneis22@gmail.com>
2024-01-12 23:54:41 +0200

    Frontend <> Edited doctor card design

--- flutter_app/lib/widgets/doctor_card.dart
+++ flutter_app/lib/widgets/doctor_card.dart
@@ -21,27 +21,107 @@ class DoctorCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(10.0),
-      child: ListTile(
-        leading: Image.asset(
-          'assets/doctor_image.png',
-          fit: BoxFit.cover,
+      color: Colors.white,
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.asset(
+                    'assets/doctor_image.png',
+                    fit: BoxFit.cover,
+                    width: 80.0,
+                    height: 80.0,
+                  ),
+                ),
+                const SizedBox(width: 10.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Dr. $name",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        specialty,
+                        style: const TextStyle(
+                            fontSize: 14, color: Color(0xFF0D4C92)),
+                      ),
+                      const SizedBox(height: 4.0),
+                      Row(
+                        children: [
+                          const Icon(Icons.access_time,
+                              color: Colors.grey, size: 16),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            '$experience Years',
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          const SizedBox(width: 25.0),
+                          const Icon(Icons.star, color: Colors.grey, size: 16),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            '$rating',
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const Divider(color: Color.fromARGB(255, 228, 225, 225)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  fee,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFF0D4C92), // Background color
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const DoctorProfileScreen(),
+                      ),
+                    );
+                  },
+                  child:
+                      const Text('Book', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ],
         ),
-        title: Text(name),
-        subtitle: Text('$specialty\n$experience Years '),
-        trailing: Text(
-          fee,
-          style: const TextStyle(
-              fontWeight: FontWeight.w900, fontSize: 18, color: Colors.black),
-        ),
-        isThreeLine: true,
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => const DoctorProfileScreen()),
-          );
-        },
       ),
     );
   }
