0ffddd8c1563450839a125d1f7ee354d37be8e77
marwaoneis <marwa.oneis22@gmail.com>
2024-01-14 18:11:41 +0200

    Frontend <> Imported patient profile screen icons

--- flutter_app/lib/screens/patient_profile.dart
+++ flutter_app/lib/screens/patient_profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PatientProfileScreen extends StatelessWidget {
   const PatientProfileScreen({super.key});
@@ -18,7 +18,11 @@ class PatientProfileScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         actions: [
           IconButton(
-            icon: const Icon(Icons.email_outlined, color: Color(0xFF0D4C92)),
+            icon: const Icon(
+              Icons.email_outlined,
+              color: Color(0xFF0D4C92),
+              size: 30,
+            ),
             onPressed: () {
               // TODO: Add edit profile action
             },
@@ -33,27 +37,27 @@ class PatientProfileScreen extends StatelessWidget {
             const SizedBox(height: 20),
             _buildProfileCard(),
             const SizedBox(height: 20),
-            _buildOption(
-                Icons.history, 'Medical History', 'Check Your Medical History',
-                onTap: () {
+            _buildOption("assets/medical.svg", 'Medical History',
+                'Check Your Medical History', onTap: () {
               // TODO: Add Medical History action
             }),
-            _buildOption(Icons.medical_services, 'Medicine & Supplements', '',
+            _buildOption('assets/medicine.svg', 'Medicine & Supplements', '',
                 onTap: () {
               // TODO: Add Medicine & Supplements action
             }),
-            _buildOption(FontAwesomeIcons.weightScale, 'Weight and Height', '',
+            _buildOption('assets/weight_scale.svg', 'Weight and Height', '',
                 onTap: () {
               // TODO: Add Weight and Height action
             }),
-            _buildOption(Icons.person, 'Personal Information', '', onTap: () {
+            _buildOption('assets/personal_info.svg', 'Personal Information', '',
+                onTap: () {
               // TODO: Add Personal Information action
             }),
-            _buildOption(Icons.health_and_safety, 'Symptom Checker', '',
+            _buildOption('assets/health_safety.svg', 'Symptom Checker', '',
                 onTap: () {
               // TODO: Add Symptom Checker action
             }),
-            _buildOption(Icons.logout, 'Log Out', '', onTap: () {
+            _buildOption('assets/logout.svg', 'Log Out', '', onTap: () {
               // TODO: Add Log Out action
             }),
           ],
@@ -63,10 +67,21 @@ class PatientProfileScreen extends StatelessWidget {
   }
 
   Widget _buildProfileCard() {
-    return Card(
+    return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 3,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey.shade400),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
       child: const Padding(
         padding: EdgeInsets.all(16),
         child: Row(
@@ -82,45 +97,72 @@ class PatientProfileScreen extends StatelessWidget {
                 children: [
                   Text('Cooper, Birnard',
                       style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Text('04/12/1978', style: TextStyle(color: Colors.grey)),
-                  Text('(316) 555-0116', style: TextStyle(color: Colors.grey)),
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                  Text('04/12/1978',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500)),
+                  Text('(316) 555-0116',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500)),
                 ],
               ),
             ),
-            Icon(Icons.edit, color: Color(0xFF0D4C92)),
+            Icon(
+              Icons.edit,
+              color: Color(0xFF0D4C92),
+              size: 30,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildOption(IconData icon, String title, String subtitle,
+  Widget _buildOption(String svgAssetPath, String title, String subtitle,
       {VoidCallback? onTap}) {
     Color iconBackgroundColor = const Color(0xFF0D4C92).withOpacity(0.2);
-    Color iconColor = const Color(0xFF0D4C92);
+    // Color iconColor = const Color(0xFF0D4C92);
 
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.grey.shade400),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: ListTile(
-        leading: Container(
-          padding: const EdgeInsets.all(8),
+    return Center(
+      child: SizedBox(
+        height: 85,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
           decoration: BoxDecoration(
-            color: iconBackgroundColor,
-            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(10),
           ),
-          child: Icon(icon, color: iconColor, size: 24),
+          child: Center(
+            child: ListTile(
+              leading: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: iconBackgroundColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: SvgPicture.asset(
+                  svgAssetPath,
+                  // width: 32,
+                  // height: 32,
+                ),
+              ),
+              title: Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              subtitle: subtitle.isNotEmpty ? Text(subtitle) : null,
+              trailing: const Icon(Icons.arrow_forward_ios,
+                  size: 16, color: Color(0xFF0D4C92)),
+              onTap: onTap,
+            ),
+          ),
         ),
-        title: Text(title),
-        subtitle: subtitle.isNotEmpty ? Text(subtitle) : null,
-        trailing: const Icon(Icons.arrow_forward_ios,
-            size: 16, color: Color(0xFF0D4C92)),
-        onTap: onTap,
       ),
     );
   }
