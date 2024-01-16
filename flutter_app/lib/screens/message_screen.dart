101a756fab711eaa23aa5857d82cb08ce39a65cd
marwaoneis <marwa.oneis22@gmail.com>
2024-01-16 23:51:42 +0200

    Frontend <> Cleaned code in messages screen

--- flutter_app/lib/screens/message_screen.dart
+++ flutter_app/lib/screens/message_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/login_screen.dart';
 import 'package:flutter_app/screens/patient_profile.dart';
 import 'package:flutter_app/screens/patient_screen.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import '../widgets/search_bar.dart';
 import '../widgets/message_list.dart';
 import '../widgets/footer.dart';
+import '../widgets/top_bar_with_background.dart';
 
 class MessageScreen extends StatelessWidget {
   const MessageScreen({super.key});
@@ -15,7 +13,28 @@ class MessageScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          _buildTopBarWithBackground(context),
+          TopBarWithBackground(
+            leadingContent: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            titleContent: const Text(
+              'Messages',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            trailingContent: IconButton(
+              icon: const Icon(Icons.filter_list, color: Colors.white),
+              onPressed: () {
+                // TODO: Sort action
+              },
+            ),
+          ),
           const Expanded(
             child: MessageList(),
           ),
@@ -48,59 +67,6 @@ class MessageScreen extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-
-  Widget _buildTopBarWithBackground(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 175,
-          decoration: const BoxDecoration(
-            color: Color(0xFF0D4C92),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.elliptical(50, 30),
-              bottomRight: Radius.elliptical(50, 20),
-            ),
-          ),
-        ),
-        Column(
-          children: [
-            const SizedBox(height: 50),
-            Container(
-              color: Colors.white.withOpacity(0.9),
-              height: 45,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text('Message',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black)),
-                    IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/back_arrow.svg',
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 45),
-            const CustomSearchBar(),
-          ],
-        ),
-      ],
     );
   }
 }
