4b554e79c4be87cd46b2a18a5d2d14c3a616b2c7
marwaoneis <marwa.oneis22@gmail.com>
2024-01-17 00:26:19 +0200

    Frontend <> Edited appointment card

--- flutter_app/lib/widgets/doctor_appointment_card.dart
+++ flutter_app/lib/widgets/doctor_appointment_card.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/appointment_model.dart'; // Ensure you have this file
 
 class AppointmentCard extends StatelessWidget {
-  final Appointment appointment;
+  final String name;
+  final String details;
+  final String status;
+  final Color statusColor;
 
   const AppointmentCard({
     super.key,
-    required this.appointment,
+    required this.name,
+    required this.details,
+    required this.status,
+    required this.statusColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(10.0),
       child: ListTile(
-        leading: Icon(Icons.person), // Replace with doctor's image if available
-        title: Text(appointment.patientName), // Replace with actual data
-        subtitle: Text(
-            'Appointment at ${appointment.time}'), // Replace with actual data
-        trailing: ElevatedButton(
-          onPressed: () {
-            // Add your onPressed logic here
-          },
-          child: Text(appointment.status), // Replace with actual data
-          style: ElevatedButton.styleFrom(
-            primary:
-                appointment.status == 'Confirmed' ? Colors.green : Colors.red,
-          ),
+        leading: const CircleAvatar(
+          // Placeholder for patient image
+          child: Icon(Icons.person),
+        ),
+        title: Text(name),
+        subtitle: Text(details),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (status == 'Confirmed') ...[
+              Icon(Icons.check, color: statusColor),
+            ] else if (status == 'Declined') ...[
+              Icon(Icons.close, color: statusColor),
+            ],
+            // Add more conditions for different statuses if needed
+          ],
         ),
       ),
     );
