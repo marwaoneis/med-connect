6be630a97251655125f4d94f33bbad197e9195a8
marwaoneis <marwa.oneis22@gmail.com>
2024-01-17 04:11:19 +0200

    Frontend <> Added top bar to doctor dashboard

--- flutter_app/lib/screens/doctor_screen.dart
+++ flutter_app/lib/screens/doctor_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../widgets/appointment_list.dart';
 import '../widgets/doctor_appointment_card.dart';
+import '../widgets/no_glow_scroll.dart';
+import '../widgets/top_bar_with_background.dart';
 
 class DoctorScreen extends StatelessWidget {
   const DoctorScreen({super.key});
@@ -8,30 +11,56 @@ class DoctorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Doctor Screen'),
-      ),
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: AppointmentList(
-            appointments: [
-              AppointmentCard(
-                name: 'Patient Name',
-                details: 'Age, Gender, Date, Time of request',
-                status: 'Confirmed',
-                statusColor: Colors.green,
+      body: Column(
+        children: <Widget>[
+          TopBarWithBackground(
+            leadingContent: CircleAvatar(
+              child: Text(
+                'D',
+                style: const TextStyle(color: Color(0xFF0D4C92)),
               ),
-              AppointmentCard(
-                name: 'Patient Name',
-                details: 'Age, Gender, Date, Time of request',
-                status: 'Declined',
-                statusColor: Colors.red,
+            ),
+            titleContent: Text(
+              'Hello Dr',
+              style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white),
+            ),
+            trailingContent: IconButton(
+              icon: SvgPicture.asset(
+                'assets/notification_icon.svg',
+                color: Colors.white,
               ),
-              // Add more appointment cards...
-            ],
+              onPressed: () {},
+            ),
           ),
-        ),
+          const Expanded(
+            child: NoGlowScrollWrapper(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: AppointmentList(
+                    appointments: [
+                      AppointmentCard(
+                        name: 'Patient Name',
+                        details: 'Age, Gender, Date, Time of request',
+                        status: 'Confirmed',
+                        statusColor: Colors.green,
+                      ),
+                      AppointmentCard(
+                        name: 'Patient Name',
+                        details: 'Age, Gender, Date, Time of request',
+                        status: 'Declined',
+                        statusColor: Colors.red,
+                      ),
+                      // Add more appointment cards...
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
