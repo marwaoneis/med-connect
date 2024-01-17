afd4eb43b587fded0c3dafe3ca7c53915b60938b
marwaoneis <marwa.oneis22@gmail.com>
2024-01-18 01:37:55 +0200

    Frontend <> Edited buttons and item list builder

--- flutter_app/lib/screens/appointments_schedule.dart
+++ flutter_app/lib/screens/appointments_schedule.dart
@@ -20,7 +20,7 @@ class AppointmentScheduleScreen extends StatelessWidget {
               },
             ),
             titleContent: const Text(
-              'Your Appointments',
+              'Appointments Schedule',
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
             trailingContent: IconButton(
@@ -35,19 +35,30 @@ class AppointmentScheduleScreen extends StatelessWidget {
           ),
           Expanded(
             child: NoGlowScrollWrapper(
-              child: ListView(
-                children: [
-                  _buildDateSection('Today'),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 15.0),
+                child: ListView(
+                  children: [
+                    const Text(
+                      'Your Appointments',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    _buildDateSection('Today'),
 
-                  const AppointmentItem(
-                    patientName: 'John Doe',
-                    appointmentTime: '09:00 AM',
-                    appointmentDate: '2023-07-21',
-                  ),
-                  // Repeat AppointmentItem for other appointments
-                  _buildDateSection('Tomorrow'),
-                  // Repeat for other dates
-                ],
+                    const AppointmentItem(
+                      patientName: 'John Doe',
+                      appointmentTime: '09:00 AM',
+                      appointmentDate: '2023-07-21',
+                    ),
+                    // Repeat AppointmentItem for other appointments
+                    _buildDateSection('Tomorrow'),
+                    // Repeat for other dates
+                  ],
+                ),
               ),
             ),
           ),
@@ -57,12 +68,10 @@ class AppointmentScheduleScreen extends StatelessWidget {
   }
 
   Widget _buildDateSection(String date) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Text(
-        date,
-        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
+    return Text(
+      date,
+      style: const TextStyle(
+          fontSize: 20, color: Colors.grey, fontWeight: FontWeight.bold),
     );
   }
 }
@@ -82,81 +91,79 @@ class AppointmentItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+      // margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
       child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
-          child: Row(
-            children: [
-              const CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://via.placeholder.com/150'), // Replace with actual patient image URL
-                radius: 20,
-              ),
-              const SizedBox(width: 22),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          const Text(
-                            'Patient',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(patientName, style: TextStyle(fontSize: 14)),
-                          _buildActionButton(context, 'Edit'),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            'Time',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(appointmentTime, style: TextStyle(fontSize: 14)),
-                          _buildActionButton(context, 'Cancel'),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            'Date',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(appointmentDate, style: TextStyle(fontSize: 14)),
-                          _buildActionButton(context, 'Medical History'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: SvgPicture.asset(
-                      "assets/chat_icon.svg",
-                      height: 20,
-                      width: 20,
-                      color: const Color(0xFF0D4C92),
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 5.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+              radius: 20,
+            ),
+            const SizedBox(width: 10),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          'Patient',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(patientName, style: TextStyle(fontSize: 14)),
+                        _buildActionButton(context, 'Edit'),
+                      ],
                     ),
-                    onPressed: () {
-                      // TODO: Implement message action
-                    },
-                  ),
-                ],
-              ),
-            ],
-          )),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          'Time',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(appointmentTime, style: TextStyle(fontSize: 14)),
+                        _buildActionButton(context, 'Cancel'),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          'Date',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(appointmentDate, style: TextStyle(fontSize: 14)),
+                        _buildActionButton(context, 'Medical History'),
+                      ],
+                    ),
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        "assets/chat_icon.svg",
+                        height: 25,
+                        width: 25,
+                        color: const Color(0xFF0D4C92),
+                      ),
+                      onPressed: () {
+                        // TODO: Implement message action
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -168,8 +175,8 @@ class AppointmentItem extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color(0xFF0D4C92),
         foregroundColor: Colors.white,
-        textStyle: const TextStyle(fontSize: 12),
-        padding: const EdgeInsets.symmetric(horizontal: 12),
+        textStyle: const TextStyle(fontSize: 14),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
