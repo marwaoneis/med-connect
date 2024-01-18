fead6db9f8541df65683f41b0f637c53e1193afc
marwaoneis <marwa.oneis22@gmail.com>
2024-01-18 02:15:15 +0200

    Frontend <> Edited importing in main file

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/patient_dashboard_screen.dart';
+import 'screens/patient_dashboard_screen.dart';
+import 'screens/pharmacy_dashboard_screen.dart';
 import 'screens/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'screens/welcome_screen.dart';
-// import 'screens/patient_screen.dart';
 import 'screens/doctor_dashboard_screen.dart';
-import 'screens/pharmacy_screen.dart';
-// import 'screens/message_screen.dart';
 import 'providers/user_provider.dart';
 
 void main() {
@@ -35,7 +33,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => const LoginScreen(),
           '/patientScreen': (context) => const PatientScreen(),
           '/doctorScreen': (context) => const DoctorScreen(),
-          '/pharmacyScreen': (context) => const PharmacyScreen(),
+          '/pharmacyScreen': (context) => const PharmacyDashboard(),
         },
       ),
     );
