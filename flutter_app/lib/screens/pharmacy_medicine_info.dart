2066db84c34334f0611159a098d3d470e418ffed
marwaoneis <marwa.oneis22@gmail.com>
2024-01-19 21:37:36 +0200

    Frontend <> Edited design and fetched data

--- flutter_app/lib/screens/pharmacy_medicine_info.dart
+++ flutter_app/lib/screens/pharmacy_medicine_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/medicine_model.dart';
+import '../widgets/top_bar_with_background.dart';
 
 class PharmacyMedicineInfoScreen extends StatelessWidget {
   final Medicine medicine;
@@ -9,25 +10,117 @@ class PharmacyMedicineInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(medicine.medicineDetails.first.name),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text('Medicine ID: ${medicine.id}'),
-                  Text(
-                      'Medicine Group: ${medicine.medicineDetails.first.group}'),
-                ],
+      body: Column(
+        children: [
+          TopBarWithBackground(
+            leadingContent: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            titleContent: Text(
+              medicine.medicineDetails.first.name,
+              style: const TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            trailingContent: Container(),
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0D4C92).withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          'Medicine',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      IntrinsicHeight(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xFF0D4C92).withOpacity(0.2),
+                                ),
+                                padding: const EdgeInsets.all(20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      medicine.id,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    const Text(
+                                      'Medicine ID',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                color: const Color(0xFF0D4C92).withOpacity(0.2),
+                                padding: const EdgeInsets.all(20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      medicine.medicineDetails.first.group,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    Text(
+                                      medicine.medicineDetails.first.group,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            // ... Rest of the UI components
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
