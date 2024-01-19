d8232994e8415252a01342593b93a4e257ef6ce8
marwaoneis <marwa.oneis22@gmail.com>
2024-01-19 16:23:27 +0200

    Frontend <> Edited medicine list design

--- flutter_app/lib/screens/medicine_list.dart
+++ flutter_app/lib/screens/medicine_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/widgets/top_bar_with_background.dart';
 import '../api/api_service.dart';
 import '../config/request_config.dart';
 import '../models/medicine_model.dart';
+import '../widgets/no_glow_scroll.dart';
 
 class MedicineListScreen extends StatefulWidget {
   final String pharmacyId;
@@ -34,27 +35,6 @@ class MedicineListScreenState extends State<MedicineListScreen> {
     return List<Medicine>.from(response.map((x) => Medicine.fromJson(x)));
   }
 
-  Widget _buildMedicineRow(Medicine medicine) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(medicine.medicineDetails.first.name),
-          Text(medicine.id),
-          Text(medicine.medicineDetails.first.group),
-          Text('${medicine.stockLevel}'),
-          ElevatedButton(
-            onPressed: () {
-              // View details logic
-            },
-            child: const Text('View Full Detail'),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,13 +63,37 @@ class MedicineListScreenState extends State<MedicineListScreen> {
                   return const Center(child: Text('No medicines found'));
                 }
 
-                List<Medicine> medicines = snapshot.data!;
-
-                return ListView.builder(
-                  itemCount: medicines.length,
-                  itemBuilder: (context, index) {
-                    return _buildMedicineRow(medicines[index]);
-                  },
+                return NoGlowScrollWrapper(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: const [
+                        DataColumn(label: Text('Medicine Name')),
+                        DataColumn(label: Text('Medicine ID')),
+                        DataColumn(label: Text('Group Name')),
+                        DataColumn(label: Text('Qty in Stock')),
+                        DataColumn(label: Text('Action')),
+                      ],
+                      rows: snapshot.data!
+                          .map(
+                            (medicine) => DataRow(cells: [
+                              DataCell(
+                                  Text(medicine.medicineDetails.first.name)),
+                              DataCell(Text(medicine.id)),
+                              DataCell(
+                                  Text(medicine.medicineDetails.first.group)),
+                              DataCell(Text('${medicine.stockLevel}')),
+                              DataCell(ElevatedButton(
+                                onPressed: () {
+                                  // TODO: View details logic
+                                },
+                                child: const Text('View Full Detail'),
+                              )),
+                            ]),
+                          )
+                          .toList(),
+                    ),
+                  ),
                 );
               },
             ),
