b9b02ffa4796aa78b6a950458aaabfc706639714
marwaoneis <marwa.oneis22@gmail.com>
2024-01-22 01:26:30 +0200

    FLutter <> EDited navigation in popular specialisst widget

--- flutter_app/lib/widgets/popular_specialties.dart
+++ flutter_app/lib/widgets/popular_specialties.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../api/api_service.dart';
 import '../config/request_config.dart';
+import '../screens/specialist_doctors.dart';
 import 'no_glow_scroll.dart';
 
 class PopularSpecialtiesWidget extends StatelessWidget {
@@ -11,10 +12,21 @@ class PopularSpecialtiesWidget extends StatelessWidget {
     var headers = RequestConfig.getHeaders(context);
     final apiService =
         ApiService(baseUrl: 'http://10.0.2.2:3001', headers: headers);
-    final data = await apiService.fetchData('doctors/specializations/');
+    final data = await apiService.fetchData('specializations/');
     return List<String>.from(data)
         .map((name) => Specialty(name, getIconPathForSpecialty(name)))
         .toList();
+  }
+
+  void _navigateToSpecialistDoctorsScreen(
+      BuildContext context, String specialization) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            SpecialistDoctorsScreen(specialization: specialization),
+      ),
+    );
   }
 
   String getIconPathForSpecialty(String specialtyName) {
@@ -61,37 +73,42 @@ class PopularSpecialtiesWidget extends StatelessWidget {
                     itemCount: specialties.length,
                     itemBuilder: (context, index) {
                       var specialty = specialties[index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              width: 60,
-                              height: 60,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF0D4C92).withOpacity(0.2),
-                                shape: BoxShape.circle,
+                      return InkWell(
+                        onTap: () => _navigateToSpecialistDoctorsScreen(
+                            context, specialty.name),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                width: 60,
+                                height: 60,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xFF0D4C92).withOpacity(0.2),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  specialty.iconPath,
+                                  semanticsLabel: specialty.name,
+                                  width: 35,
+                                  height: 35,
+                                ),
                               ),
-                              child: SvgPicture.asset(
-                                specialty.iconPath,
-                                semanticsLabel: specialty.name,
-                                width: 35,
-                                height: 35,
+                              const SizedBox(height: 5),
+                              Text(
+                                specialty.name,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13.0,
+                                ),
                               ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              specialty.name,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 13.0,
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       );
                     },
