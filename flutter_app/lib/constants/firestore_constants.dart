957e969043ef0dcab8db7945b08f887dd887727d
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 02:12:19 +0200

    Frontend <> Implemented new folder and file for firestore constant values

---
+++ flutter_app/lib/constants/firestore_constants.dart
@@ -0,0 +1,19 @@
+class FirestoreConstants {
+  static const pathUserCollection = "users";
+  static const pathMessageCollection = "messages";
+  static const pathChatsCollection = "chats";
+  static const firstName = "firstName";
+  static const lastName = "lastName";
+  static const fullName = "fullName";
+  static const phoneNumber = "phoneNumber";
+  static const id = "id";
+  static const idFrom = "idFrom";
+  static const idTo = "idTo";
+  static const timestamp = "timestamp";
+  static const type = "type";
+  static const dateOfBirth = "dateOfBirth";
+  static const gender = "gender";
+  static const additionalInfo = "additionalInfo";
+  static const content = "content";
+  static const token = "token";
+}
