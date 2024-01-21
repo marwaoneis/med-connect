b983404b1a32b70720d1ef6cd827022ec00a2327
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 02:23:54 +0200

    Frontend <> Imported the providers in the message screen

--- flutter_app/lib/screens/message_screen.dart
+++ flutter_app/lib/screens/message_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/providers/auth_provider.dart';
+import 'package:flutter_app/providers/chat_provider.dart';
+import 'package:flutter_app/providers/chats_provider.dart';
 import 'package:flutter_app/screens/patient_profile.dart';
 import 'package:flutter_app/screens/patient_dashboard_screen.dart';
+import 'package:provider/provider.dart';
 import '../widgets/message_list.dart';
 import '../widgets/footer.dart';
 import '../widgets/top_bar_with_background.dart';
+import 'login_screen.dart';
 
-class MessageScreen extends StatelessWidget {
+class MessageScreen extends StatefulWidget {
   const MessageScreen({super.key});
+
+  @override
+  State<MessageScreen> createState() => _MessageScreen();
+}
+
+class _MessageScreen extends State<MessageScreen> {
+  final ScrollController scrollController = ScrollController();
+
+  late ChatsProvider chatsProvider;
+  late ChatProvider chatProvider;
+  late Auth authProvider;
+  late String currentUserId;
+
+  @override
+  void initState() {
+    super.initState();
+    authProvider = context.read<Auth>();
+    chatsProvider = context.read<ChatsProvider>();
+    chatProvider = context.read<ChatProvider>();
+    if (authProvider.getUserId()?.isNotEmpty == true) {
+      currentUserId = authProvider.getUserId()!;
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (Route<dynamic> route) => false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
