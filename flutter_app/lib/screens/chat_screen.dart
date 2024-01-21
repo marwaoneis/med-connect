d21b37394f1e6247e44164ea1018bd4102efc092
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 02:32:10 +0200

    Frontend <> Implemented the chating system in the chat screen using providers and firestore

--- flutter_app/lib/screens/chat_screen.dart
+++ flutter_app/lib/screens/chat_screen.dart
@@ -1,29 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/providers/auth_provider.dart';
+import 'package:flutter_app/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/chat_provider.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  final String receiverId;
+  final String receiverName;
+
+  const ChatScreen(
+      {super.key, required this.receiverId, required this.receiverName});
 
   @override
   State createState() => ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
-  final List<Message> _messages = [];
+  late String senderId;
+  late String senderName;
+  late String receiverName;
+  late ChatProvider chatProvider;
+  late Auth authProvider;
+  late List<Message> _messages = [];
   final TextEditingController _textController = TextEditingController();
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    authProvider = context.read<Auth>();
+    chatProvider = context.read<ChatProvider>();
+    _scrollController = ScrollController();
+
+    if (authProvider.getUserId()?.isNotEmpty == true) {
+      senderId = authProvider.getUserId()!;
+      senderName =
+          authProvider.getUserId()!; // This should be replaced with userName
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (Route<dynamic> route) => false);
+    }
+
+    chatProvider
+        .getChatMessages(senderId, widget.receiverId, 10)
+        .listen((QuerySnapshot snapshot) {
+      List<Message> newMessages = [];
+
+      for (QueryDocumentSnapshot doc in snapshot.docs) {
+        // Assuming you have a method to convert a document to a Message
+        var messageContent = doc.data() as Map<String, dynamic>;
+        Message message = Message(
+            text: messageContent['content']!,
+            animationController: AnimationController(
+              duration: const Duration(milliseconds: 700),
+              vsync: this,
+            ));
+        newMessages.add(message);
+      }
+
+      setState(() {
+        _messages = newMessages;
+      });
+    });
+  }
 
   void _handleSubmitted(String text) {
-    _textController.clear();
-    Message message = Message(
-      text: text,
-      animationController: AnimationController(
-        duration: const Duration(milliseconds: 700),
-        vsync: this,
-      ),
-    );
-    setState(() {
-      _messages.insert(0, message);
-    });
-    message.animationController.forward();
+    if (text.trim().isNotEmpty) {
+      _textController.clear();
+      Message message = Message(
+        text: text,
+        animationController: AnimationController(
+          duration: const Duration(milliseconds: 700),
+          vsync: this,
+        ),
+      );
+      setState(() {
+        _messages.insert(0, message);
+      });
+      message.animationController.forward();
+
+      // Scroll to the newly added message
+      _scrollController.animateTo(
+        0.0,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+
+      chatProvider.sendChatMessage(text, 0, senderId, widget.receiverId,
+          widget.receiverName, senderName);
+    } else {
+      // TODO: IMPLEMENT WHAT TO DO IN CASE MESSAGE WAS EMPTY
+    }
   }
 
   Widget _buildTextComposer() {
@@ -56,15 +126,15 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dr. Jamal Khan')),
+      appBar: AppBar(title: Text(widget.receiverName)),
       body: Column(
         children: <Widget>[
           Flexible(
-            child: ListView.builder(
+            child: ListView(
+              controller: _scrollController,
               padding: const EdgeInsets.all(8.0),
               reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+              children: _messages,
             ),
           ),
           const Divider(height: 1.0),
@@ -87,8 +157,10 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
 }
 
 class Message extends StatelessWidget {
-  const Message(
-      {required this.text, required this.animationController, super.key});
+  Message({required this.text, required this.animationController, super.key}) {
+    animationController.forward();
+  }
+
   final String text;
   final AnimationController animationController;
 
