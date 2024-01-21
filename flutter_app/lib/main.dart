e395442e3533e1d37f3597dcd056004c7bbdbaf4
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 03:00:24 +0200

    Frontend <> Added firebase function for storing states and added providers

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'providers/chat_provider.dart';
+import 'providers/chats_provider.dart';
 import 'screens/patient_dashboard_screen.dart';
 import 'screens/pharmacy_dashboard_screen.dart';
 import 'screens/login_screen.dart';
@@ -8,19 +13,36 @@ import 'screens/welcome_screen.dart';
 import 'screens/doctor_dashboard_screen.dart';
 import 'providers/user_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp(
+    prefs: prefs,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final SharedPreferences prefs;
+
+  MyApp({super.key, required this.prefs});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => Auth()),
+        ChangeNotifierProvider(
+            create: (context) => Auth(prefs, firebaseFirestore)),
         ChangeNotifierProvider(create: (context) => UserProvider()),
+        Provider<ChatsProvider>(
+            create: (_) => ChatsProvider(firebaseFirestore: firebaseFirestore)),
+        Provider<ChatProvider>(
+            create: (_) => ChatProvider(
+                prefs: prefs, firebaseFirestore: firebaseFirestore))
       ],
       child: MaterialApp(
         title: 'MedConnect',
