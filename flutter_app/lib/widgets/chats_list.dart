ead985923bfba203decf0c33b5fd07ced458b74d
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 02:32:38 +0200

    Frontend <> Edited the chats list to fetch chating between receiver and sender

--- flutter_app/lib/widgets/chats_list.dart
+++ flutter_app/lib/widgets/chats_list.dart
@@ -1,31 +1,63 @@
 import 'package:flutter/material.dart';
+import '../constants/firestore_constants.dart';
 import '../screens/chat_screen.dart';
 
 class ChatsList extends StatelessWidget {
-  const ChatsList({super.key});
+  final Stream<List<Map<String, dynamic>>> chatsStream;
+
+  const ChatsList({super.key, required this.chatsStream});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ChatScreen()),
+    return StreamBuilder<List<Map<String, dynamic>>>(
+      stream: chatsStream,
+      builder: (BuildContext context,
+          AsyncSnapshot<List<Map<String, dynamic>>> chats) {
+        if (chats.hasData) {
+          print(chats);
+          if (chats.data!.isNotEmpty) {
+            return ListView.builder(
+              itemCount: chats.data!.length,
+              itemBuilder: (context, index) =>
+                  buildItem(context, chats.data![index]),
             );
-          },
-          child: const ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/profile_placeholder.png'),
+          } else {
+            return const Center(
+              child: Text('No messages found...'),
+            );
+          }
+        } else if (chats.hasError) {
+          return const Center(
+            child: Text('Error loading messages.'),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget buildItem(BuildContext context, Map<String, dynamic> chat) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatScreen(
+              receiverId: chat[FirestoreConstants.id],
+              receiverName: chat[FirestoreConstants.fullName],
             ),
-            title: Text('Dr Name'),
-            subtitle: Text('Message Content'),
-            trailing: Text('Date/time'),
           ),
         );
       },
+      child: ListTile(
+        leading: const CircleAvatar(
+          backgroundImage: AssetImage('assets/profile_placeholder.png'),
+        ),
+        title: Text(chat[FirestoreConstants.fullName] ?? ''),
+      ),
     );
   }
 }
