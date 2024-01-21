24bc4a7133e1558366d82d2e888643166dd67c5f
marwaoneis <marwa.oneis22@gmail.com>
2024-01-21 02:13:48 +0200

    Frontend <> Implemented chat message model

--- flutter_app/lib/providers/chat_provider.dart
+++ flutter_app/lib/providers/chat_provider.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../constants/firestore_constants.dart';
+import '../models/chat_message.dart';
 
 class ChatProvider {
   final SharedPreferences prefs;
