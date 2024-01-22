19628af12c9d4e5a71ed8b77a463b40168ab65eb
marwaoneis <marwa.oneis22@gmail.com>
2024-01-22 23:26:55 +0200

    Frontend <> Edited parameters nefore navigating to chat screen

--- flutter_app/lib/widgets/chats_list.dart
+++ flutter_app/lib/widgets/chats_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../constants/firestore_constants.dart';
+import '../providers/auth_provider.dart';
 import '../screens/chat_screen.dart';
 
 class ChatsList extends StatelessWidget {
@@ -7,8 +9,15 @@ class ChatsList extends StatelessWidget {
 
   const ChatsList({super.key, required this.chatsStream});
 
+  String getSenderName(Auth authProvider) {
+    return authProvider.getFullName ?? "Unknown";
+  }
+
   @override
   Widget build(BuildContext context) {
+    Auth authProvider = Provider.of<Auth>(context, listen: false);
+    String senderName = getSenderName(authProvider);
+
     return StreamBuilder<List<Map<String, dynamic>>>(
       stream: chatsStream,
       builder: (BuildContext context,
@@ -19,7 +28,7 @@ class ChatsList extends StatelessWidget {
             return ListView.builder(
               itemCount: chats.data!.length,
               itemBuilder: (context, index) =>
-                  buildItem(context, chats.data![index]),
+                  buildItem(context, chats.data![index], senderName),
             );
           } else {
             return const Center(
@@ -39,7 +48,8 @@ class ChatsList extends StatelessWidget {
     );
   }
 
-  Widget buildItem(BuildContext context, Map<String, dynamic> chat) {
+  Widget buildItem(
+      BuildContext context, Map<String, dynamic> chat, String senderName) {
     return InkWell(
       onTap: () {
         Navigator.push(
@@ -48,6 +58,7 @@ class ChatsList extends StatelessWidget {
             builder: (context) => ChatScreen(
               receiverId: chat[FirestoreConstants.id],
               receiverName: chat[FirestoreConstants.fullName],
+              senderName: senderName,
             ),
           ),
         );
