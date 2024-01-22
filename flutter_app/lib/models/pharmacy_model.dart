28bab98cae2eb81ec63cdd06ed9434fc93d71611
marwaoneis <marwa.oneis22@gmail.com>
2024-01-22 15:45:58 +0200

    Frontend <> Handeled location if provided in pharmacy model in dart

--- flutter_app/lib/models/pharmacy_model.dart
+++ flutter_app/lib/models/pharmacy_model.dart
@@ -2,15 +2,27 @@ class Pharmacy {
   final String username;
   final String address;
   final String phone;
+  final List<double>?
+      location; // Representing location as [longitude, latitude]
 
-  Pharmacy(
-      {required this.username, required this.address, required this.phone});
+  Pharmacy({
+    required this.username,
+    required this.address,
+    required this.phone,
+    this.location,
+  });
 
   factory Pharmacy.fromJson(Map<String, dynamic> json) {
     return Pharmacy(
       username: json['username'],
       address: json['address'],
       phone: json['phone'],
+      location: json['location'] != null
+          ? [
+              json['location']['coordinates'][0],
+              json['location']['coordinates'][1]
+            ]
+          : null,
     );
   }
 }
