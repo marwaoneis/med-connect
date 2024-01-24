222597b04bb0f83638229363eb7e6152ba8d30d6
marwaoneis <marwa.oneis22@gmail.com>
2024-01-24 20:39:26 +0200

    Frontend <> Added border box with shadow for message lists

--- flutter_app/lib/widgets/chats_list.dart
+++ flutter_app/lib/widgets/chats_list.dart
@@ -9,33 +9,51 @@ class ChatsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Map<String, dynamic>>>(
-      stream: chatsStream,
-      builder: (BuildContext context,
-          AsyncSnapshot<List<Map<String, dynamic>>> chats) {
-        if (chats.hasData) {
-          print(chats);
-          if (chats.data!.isNotEmpty) {
-            return ListView.builder(
-              itemCount: chats.data!.length,
-              itemBuilder: (context, index) =>
-                  buildItem(context, chats.data![index]),
-            );
-          } else {
-            return const Center(
-              child: Text('No messages found...'),
-            );
-          }
-        } else if (chats.hasError) {
-          return const Center(
-            child: Text('Error loading messages.'),
-          );
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 3,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: StreamBuilder<List<Map<String, dynamic>>>(
+          stream: chatsStream,
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> chats) {
+            if (chats.hasData) {
+              if (chats.data!.isNotEmpty) {
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: chats.data!.length,
+                        itemBuilder: (context, index) =>
+                            buildItem(context, chats.data![index]),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(child: Text('No messages found...'));
+              }
+            } else if (chats.hasError) {
+              return const Center(child: Text('Error loading messages.'));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
     );
   }
 
