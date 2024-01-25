39d0c7aa58753ee412fbd8e845048917012ab2b4
marwaoneis <marwa.oneis22@gmail.com>
2024-01-25 11:17:29 +0200

    Backend <> Edited routes positions

--- server/routes/appintment.routes.js
+++ server/routes/appintment.routes.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const appointmentController = require("../controllers/appointment.controller");
 
 router.post("/appointments", appointmentController.createAppointment);
+router.get(
+  "/appointments/doctor/:doctorId",
+  appointmentController.getAppointmentsByDoctorId
+);
 router.get("/appointments/:id", appointmentController.getAppointmentById);
 router.put("/appointments/:id", appointmentController.updateAppointmentById);
 router.delete("/appointments/:id", appointmentController.deleteAppointmentById);
 router.get(
   "/appointments/patient/:patientId",
   appointmentController.getAppointmentsByPatientId
-);
-router.get(
-  "/appointments/doctor/:doctorId",
-  appointmentController.getAppointmentsByDoctorId
 );
 
 module.exports = router;
