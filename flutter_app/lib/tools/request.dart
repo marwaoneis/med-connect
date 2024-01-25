c7398ebe44c5e07c24ed8deb1d32cc12c76799d5
marwaoneis <marwa.oneis22@gmail.com>
2024-01-26 01:36:06 +0200

    Frontend <> Edited delete request

--- flutter_app/lib/tools/request.dart
+++ flutter_app/lib/tools/request.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../config/request_config.dart';
 
@@ -7,9 +8,19 @@ Future sendRequest({
   method = "GET",
   load,
   context,
+  String? jwtToken,
+  String? apiKey,
 }) async {
   final url = Uri.http(RequestConfig.url, route);
   final Map<String, String> headers = RequestConfig.getHeaders(context);
+
+  if (jwtToken != null) {
+    headers['Authorization'] = 'Bearer $jwtToken';
+  }
+
+  if (apiKey != null) {
+    headers['X-API-Key'] = apiKey;
+  }
 
   if (method == "GET") {
     final response = await http.get(
@@ -45,8 +56,21 @@ Future sendRequest({
       url,
       headers: headers,
     );
-    final data = json.decode(response.body);
-    return data;
+
+    if (response.body.isNotEmpty &&
+        response.headers['content-type']?.contains('application/json') ==
+            true) {
+      final data = json.decode(response.body);
+      return data;
+    } else if (response.statusCode == 204) {
+      return {'success': true};
+    } else {
+      return {
+        'success': false,
+        'message':
+            'No content in response and status code is ${response.statusCode}'
+      };
+    }
   }
 
   return Future(() {
